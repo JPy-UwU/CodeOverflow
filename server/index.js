39b78e6ba9db4cd4046ae587b1b2a1b892ba44b1
JP <jay.patel.py@gmail.com>
2023-10-31 16:42:07 -0600

    added mysql database connection

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
 
+import db from "./db.js";
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
