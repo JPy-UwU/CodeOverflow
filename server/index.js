7cd5aa91bf88dbb002baad90f0ca901cbb5a7a9d
JP <jay.patel.py@gmail.com>
2023-10-30 23:52:26 -0600

    started an express server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
