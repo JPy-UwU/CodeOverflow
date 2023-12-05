d7bced7f728195247bacc817f3f169fd632c61f1
JP <jay.patel.py@gmail.com>
2023-12-05 13:48:22 -0600

    Add dependencies and context providers

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
 import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DarkModeProvider>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </DarkModeProvider>
   </React.StrictMode>
 );
