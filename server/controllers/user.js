f5ca097420ebb2f8136482c8b1f1676194c91e22
JP <jay.patel.py@gmail.com>
2023-12-06 11:56:04 -0600

    Add @tanstack/react-query package and implement getAllUsers endpoint

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,9 +2,9 @@ import { db } from "../utils/db.js";
 import jwt from "jsonwebtoken";
 
 export const getUser = (req, res) => {
-  const userId = req.params.userId;
-  const query = "SELECT * FROM users WHERE id=?";
-  const values = [userId];
+  const username = req.params.userId;
+  const query = "SELECT * FROM User WHERE username=?";
+  const values = [username];
 
   db.query(query, values, (err, data) => {
     if (err) 
@@ -17,12 +17,23 @@ export const getUser = (req, res) => {
 
 export const deleteUser = (req, res) => {
   const userId = req.params.userId;
-  const query = "DELETE FROM users WHERE id=?";
+  const query = "DELETE FROM User WHERE id=?";
   const values = [userId];
 
   db.query(query, values, (err, data) => {
     if (err) 
       return res.status(500).json(err);
     return res.json({ message: "User deleted successfully!" });
+  });
+};
+
+export const getAllUsers = (req, res) => {
+  const query = "SELECT * FROM User";
+
+  db.query(query, (err, data) => {
+    if (err) 
+      return res.status(500).json(err);
+    console.log(res.json(data));
+    return res.json(data);
   });
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getUser, deleteUser } from '../controllers/user.js';
+import { getUser, deleteUser, getAllUsers } from '../controllers/user.js';
 
 const router = express.Router();
 
 router.get("/find/:userId", getUser)
 router.put("/delete/:userId", deleteUser)
+router.get("/all", getAllUsers);
 
 export default router;
