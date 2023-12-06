9fa755350d9757d5c18e30fd623da325f75b5127
JP <jay.patel.py@gmail.com>
2023-12-05 19:33:07 -0600

    Refactor routing in App.js and add links in Login and Register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,38 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import LadningPage from "./pages/landing/Landing";
+import HomePage from "./pages/home/Home";
 import LoginPage from "./pages/login/Login";
 import RegisterPage from "./pages/register/Register";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LadningPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/home",
+    element: <HomePage />,
+  },
+]);
+
+
 function App() {
   return (
-    <RegisterPage />
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
