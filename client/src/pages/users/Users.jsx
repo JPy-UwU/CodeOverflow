3092e2d90aa17b49d9052369aca6b41fa5f261bc
JP <jay.patel.py@gmail.com>
2023-12-06 11:01:27 -0600

    Added users page

--- client/src/pages/users/Users.jsx
+++ client/src/pages/users/Users.jsx
@@ -1,11 +1,34 @@
-import "./users.scss"
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+
+import "./users.scss";
 
 const UsersPage = () => {
+  const users = [
+   {
+    username: "John",
+   },
+   {
+    username: "Jane",
+   },
+   {
+    username: "Joe",
+   }
+  ];
+
+  const isAdmin = true;
+
   return (
     <div className="users">
-      <h1>Users</h1>
+      <div className="container">
+        {users.map((user) => (
+          <div className="user">
+            <span>{user.username}</span>
+            {isAdmin && <i><DeleteOutlinedIcon /></i>}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
- 
+};
+
 export default UsersPage;
