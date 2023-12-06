4db18bcaca586b7f4295966c977d1a1cd48f1f90
JP <jay.patel.py@gmail.com>
2023-12-05 19:46:35 -0600

    Add profile page to router

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,39 @@ import {
 
 import LadningPage from "./pages/landing/Landing";
 import HomePage from "./pages/home/Home";
+import ProfilePage from "./pages/profile/Profile";
 import LoginPage from "./pages/login/Login";
 import RegisterPage from "./pages/register/Register";
 import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
 
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/home",
+        element: <HomePage />,
+      },
+      {
+        path: "/profile/:id",
+        element: <ProfilePage />,
+      },
+    ],
+  },
   {
     path: "/",
     element: <LadningPage />,
@@ -23,27 +50,11 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <RegisterPage />,
-  },
-  {
-    path: "/home",
-    element: <HomePage />,
-  },
+  }
 ]);
 
 
 function App() {
-  const Layout = () => {
-    return (
-      <div>
-        <Navbar />
-        <div style={{ display: "flex" }}>
-          <Sidebar />
-          <Outlet />
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div>
       <RouterProvider router={router} />
