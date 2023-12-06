eaa91ed27698fda1725378fff46d28e7797cd771
JP <jay.patel.py@gmail.com>
2023-12-05 20:45:20 -0600

    Add dark mode support and useContext hook

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {
   Outlet,
   RouterProvider,
 } from "react-router-dom";
+import { useContext } from "react";
 
 import "./style.scss";
 import LadningPage from "./pages/landing/Landing";
@@ -13,66 +14,68 @@ import ProfilePage from "./pages/profile/Profile";
 import LoginPage from "./pages/login/Login";
 import RegisterPage from "./pages/register/Register";
 import Navbar from "./components/navbar/Navbar";
-
-const currentUser = true;
-
-const Layout = () => {
-  return (
-    <div className="theme-light">
-      <Navbar />
-      <div style={{ display: "flex" }}>
-        <Outlet />
-      </div>
-    </div>
-  );
-}
-
-const ProtectedRoute = ({children}) => {
-  if (!currentUser) {
-    return <Navigate to="/login" />;
-  }
-  return children;
-};
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LadningPage />,
-  },
-  {
-    path: "/",
-    element: (
-      <ProtectedRoute>
-        <Layout />
-      </ProtectedRoute>
-    ),
-    children: [
-      {
-        path: "/home",
-        element: <HomePage />,
-      },
-      {
-        path: "/profile/:id",
-        element: <ProfilePage />,
-      },
-      {
-        path: "/post/:id",
-        element: <PostPage />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />,
-  }
-]);
-
+import { DarkModeContext } from "./context/darkModeContext";
 
 function App() {
+  const { darkMode } = useContext(DarkModeContext);
+
+  const currentUser = true;
+
+  const Layout = () => {
+    return (
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <Outlet />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LadningPage />,
+    },
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/home",
+          element: <HomePage />,
+        },
+        {
+          path: "/profile/:id",
+          element: <ProfilePage />,
+        },
+        {
+          path: "/post/:id",
+          element: <PostPage />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+  ]);
+
   return (
     <div>
       <RouterProvider router={router} />
